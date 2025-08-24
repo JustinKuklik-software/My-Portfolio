1f1f095f846363ba4182a7b5b6f805e0b4727bf8
Justin Kuklik <kuklikjustin00@gmail.com>
2025-08-24 14:11:34 +0200

    Add files via upload

--- Portfolio_Justin/script.js
+++ Portfolio_Justin/script.js
@@ -8,16 +8,13 @@ function toggleCertificates(id, cellElement, event) {
   const folderIcon = cellElement.querySelector(".folder-icon");
   const box = document.getElementById(id);
 
-  // Toggle-Klasse 'show'
   const isOpen = box.classList.contains("show");
 
   if (isOpen) {
-    // schließen
     box.classList.remove("show");
     box.style.height = "0";
     if (folderIcon) folderIcon.style.transform = "rotate(0deg)";
   } else {
-    // öffnen
     box.classList.add("show");
     const height = box.scrollHeight + "px";
     box.style.height = height;
